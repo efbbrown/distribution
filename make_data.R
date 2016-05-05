dfb912e191fbad00a84454f4eee5720f2b1647da
Eugene Brown <efbbrown@gmail.com>
2016-05-05 18:58:36 +1000

    Added a couple distributions

--- make_data.R
+++ make_data.R
@@ -1,31 +1,39 @@
+# Load necessary packages
 pacman::p_load(data.table, magrittr, EnvStats)
-
 pacman::p_load_gh("efbbrown/make")
 
-data <- make_once("data/data", function() {
+# Return three columns relating to each variable type
+var_gen <- function(r_gen, d_gen, n, dist_name, ...) {
   
+  x <- r_gen(n, ...) %>% sort
+  
+  y <- d_gen(x, ...)
+  
+  y_d <- runif(n, min = 0, max = y)
+  
+  res <- data.table(x, y, y_d)
+  
+  cols <- paste(c("x", "y", "y_d"), dist_name, sep = "_")
+  
+  setnames(res, cols)
+  
+  return(res)
+  
+}
+
+# If file already exits load it, otherwise run the function to create it
+data <- make_once("data/data", function() {
+  # The number of each variable to create, the number of rows in data
   n <- 500
   
   set.seed(100)
   
-  var_gen <- function(r_gen, d_gen, n, dist_name, ...) {
-    
-    x <- r_gen(n, ...) %>% sort
-    
-    y <- d_gen(x, ...)
-    
-    y_d <- runif(n, min = 0, max = y)
-    
-    res <- data.table(x, y, y_d)
-    
-    cols <- paste(c("x", "y", "y_d"), dist_name, sep = "_")
-    
-    setnames(res, cols)
-    
-    return(res)
-    
-  }
+  # The discrete distributions
+  poisson <- var_gen(rpois, dpois, n, "poisson", lambda = 10)
   
+  binoms <- var_gen(rbinom, dbinom, n, "binom", size = 60, prob = 0.5)
+  
+  # The continuous distributions
   normals <- var_gen(rnorm, dnorm, n, "normal")
   
   exps <- var_gen(rexp, dexp, n, "exp")
@@ -38,9 +46,8 @@ data <- make_once("data/data", function() {
   
   logis <- var_gen(rlogis, dlogis, n, "logis")
   
-  tris <- var_gen(rtri, dtri, n, "tri")
-  
-  data <- cbind(normals, exps, gammas, lognormals, logis, tris)
+  data <- cbind(normals, binoms, poisson, exps, gammas, lognormals, logis,
+                poisson)
   
   data <- round(data, digits = 4)
   
